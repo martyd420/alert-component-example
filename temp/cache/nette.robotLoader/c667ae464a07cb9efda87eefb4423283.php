360f438af102a4f7a01121514cc6bde5c5ab596b
martyd420 <github@pcdr.cz>
2022-08-29 11:02:27 +0200

    added styles in SCSS

--- temp/cache/nette.robotLoader/c667ae464a07cb9efda87eefb4423283.php
+++ temp/cache/nette.robotLoader/c667ae464a07cb9efda87eefb4423283.php
@@ -5,12 +5,12 @@ return array (
     'App\\Alert\\Alert' => 
     array (
       0 => 'E:\\src\\domaci-ukoly\\alert-component-example\\app\\Alert\\Alert.php',
-      1 => 1661688061,
+      1 => 1661757186,
     ),
     'App\\Bootstrap' => 
     array (
       0 => 'E:\\src\\domaci-ukoly\\alert-component-example\\app\\Bootstrap.php',
-      1 => 1642999500,
+      1 => 1661757939,
     ),
     'App\\Presenters\\Error4xxPresenter' => 
     array (
@@ -25,7 +25,7 @@ return array (
     'App\\Presenters\\HomepagePresenter' => 
     array (
       0 => 'E:\\src\\domaci-ukoly\\alert-component-example\\app\\Presenters\\HomepagePresenter.php',
-      1 => 1661688071,
+      1 => 1661757400,
     ),
     'App\\Router\\RouterFactory' => 
     array (
